39573649ecb25a7489de95fb2f35d1ea435d101c
Aaron McLean <aaronmclean26@gmail.com>
2024-11-17 18:10:37 -0500

    11/18/24

--- Server/repbook.js
+++ Server/repbook.js
@@ -24,8 +24,8 @@ app.use((req, res, next) => {
   const pool = new Pool({
     user: process.env.DB_USER || "postgres",
     host: process.env.DB_HOST || "localhost",
-    database: process.env.DB_NAME || "aaronmclean",
-    password: process.env.DB_PASSWORD || "Apple1206",
+    database: process.env.DB_NAME || "",
+    password: process.env.DB_PASSWORD || "",
     port: process.env.DB_PORT || 4126,
 });
 /**
